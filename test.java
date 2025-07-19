2b4ea03a946a1a75557e5e4cc952222e6a3fb9c7
jrin-jsph <jerrin.joseph401@gmail.com>
2025-07-19 23:36:11 +0530

    #

--- test.java
+++ test.java
@@ -1,33 +1,11 @@
-
-/**
- * Write a description of class test here.
- *
- * @author (your name)
- * @version (a version number or a date)
- */
-public class test
+class test
 {
-    // Hello instance variables madhav - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class test
-     */
-    public test()
+    public static void main()
     {
-        // initialise instance variables
-        x = 0;
+        int a=71;
+        int b=3;
+        int c=a+b;
+        System.out.println(c);
     }
-
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
-    }
+    
 }
